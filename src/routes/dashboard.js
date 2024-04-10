0463d7cb2b39fe0acb07c2d3cf0eb83ad7dc7fbf
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2024-04-10 19:32:46 +0100

    feat: intergating dashboard client

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,8 +1,19 @@
 import express from "express";
 const dashboardRouter = express.Router();
 
-dashboardRouter.get("", (req, res) => {
-  res.render("member/dashboard", { title: "Dashboard" });
+const middlewares = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect("/auth/login");
+};
+
+dashboardRouter.get("", middlewares, (req, res) => {
+  const user = req.user;
+  const data = {
+    user,
+  };
+  res.render("member/dashboard", { title: "Dashboard", data: data });
 });
 
 export default dashboardRouter;
