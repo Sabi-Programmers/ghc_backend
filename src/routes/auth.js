1890a2224ced2efe2c15d2d3da2e478efb0db85b
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2024-04-10 19:48:25 +0100

    feat: logout implemented

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -29,5 +29,13 @@ authRouter.post(
     failureRedirect: "/auth/login",
   })
 );
+authRouter.post("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 export default authRouter;
