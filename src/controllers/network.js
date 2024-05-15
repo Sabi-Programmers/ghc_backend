4549248598d2ab1de452a62162833bc335fda3fe
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2024-05-15 14:17:18 +0100

    my referrers done

--- src/controllers/network.js
+++ src/controllers/network.js
@@ -40,4 +40,34 @@ const getReferrerTreePage = asyncWrapper(async (req, res) => {
   });
 });
 
-export { getReferrerTreePage };
+const getMyReferrersPage = asyncWrapper(async (req, res) => {
+  const data = {
+    user: req.user,
+  };
+
+  const page = Number(req.query.page) || 1; // Current page
+  const perPage = Number(req.query.limit) || 10; // Number of records per page
+
+  data.downlines = await database.user.findMany({
+    where: {
+      sponsorId: req.user.id,
+    },
+    include: { bronze: true, gold: true, diamond: true },
+    skip: (page - 1) * perPage,
+    take: perPage,
+    orderBy: { createdAt: "desc" },
+  });
+
+  const totalItem = await database.user.count({
+    where: { sponsorId: req.user.id },
+  });
+
+  data.pagination = calculatePagination(totalItem, page, perPage);
+
+  res.render("member/my-network/my-referrers", {
+    title: "My Referrers",
+    data,
+  });
+});
+
+export { getReferrerTreePage, getMyReferrersPage };
--- src/routes/network.js
+++ src/routes/network.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getReferrerTreePage } from "../controllers/network.js";
+import {
+  getReferrerTreePage,
+  getMyReferrersPage,
+} from "../controllers/network.js";
 
 const networkRouter = express.Router();
 
 networkRouter.get("/referrer-tree", getReferrerTreePage);
+networkRouter.get("/my-referrers", getMyReferrersPage);
 
 export default networkRouter;
