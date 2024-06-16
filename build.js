55476176c331985536b8fa84bf9bf469638926e8
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2024-06-16 17:51:53 +0100

    last working

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 // build.js
 const esbuild = require('esbuild')
+const { copy } = require('esbuild-plugin-copy')
 
 esbuild
     .build({
@@ -9,5 +10,16 @@ esbuild
         outfile: './dist/index.js', // Output file
         platform: 'node', // Platform target
         target: ['node18'], // Target environment
+        plugins: [
+            copy({
+                resolveFrom: 'node_modules/.prisma/client',
+                to: 'dist/node_modules/.prisma/client',
+                assets: {
+                    // Copy the specific binary for your environment
+                    from: 'libquery_engine-debian-openssl-1.0.x.so.node',
+                    to: 'libquery_engine-debian-openssl-1.0.x.so.node',
+                },
+            }),
+        ],
     })
     .catch(() => process.exit(1))
